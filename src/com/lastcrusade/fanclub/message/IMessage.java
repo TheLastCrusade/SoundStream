2e5ae8354f7612b2f1f8fcecbd9534f1741d6452
David Greenhalgh <david@david-desktop.(none)>
2013-02-02 10:08:27 -0500

    Getting repos sorted out

---
+++ src/com/lastcrusade/fanclub/message/IMessage.java
@@ -0,0 +1,30 @@
+package com.lastcrusade.fanclub.message;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+
+public interface IMessage {
+
+    /**
+     * Get the message version, which is used to determine wireline compatibility.
+     * 
+     * @return
+     */
+    public int getMessageVersion();
+
+    /**
+     * Deserialize a message that has been received in an input stream.  This will be
+     * called if a message of this type is received.
+     * 
+     * NOTE: it can be assumed that this input stream has all required data and therefore
+     * will not block.  The framework will ensure this.
+     * 
+     * @param input
+     * @throws IOException 
+     */
+    public void deserialize(InputStream  input) throws IOException;
+
+    public void serialize(  OutputStream output) throws IOException;
+}
