04868c002fb3e5c47d683065e7dd7c3feea2d484
Jesse Rosalia <jesse.rosalia@gatech.edu>
2013-02-18 13:45:27 -0500

    Added comments explaining the polling process in checkAndEnableBluetooth. Moved wait time values into static final constants

--- fanClub/src/com/lastcrusade/fanclub/util/BluetoothUtils.java
+++ fanClub/src/com/lastcrusade/fanclub/util/BluetoothUtils.java
@@ -10,23 +10,32 @@ import com.lastcrusade.fanclub.R;
 
 public class BluetoothUtils {
     /* All methods should be static */
-    private final String TAG = "BluetoothUtils";
+    private static final String TAG = "BluetoothUtils";
+    
+    /* The maximum number of seconds in which to wait for bluetooth to enable */
+    private static final int ENABLE_CHECK_RETRY_SECONDS = 5;
+    
+    /* The number of milliseconds to wait in between calls to isEnabled */
+    private static final int ENABLE_CHECK_RETRY_PAUSE_MILLISECONDS = 500;
 
     public static void checkAndEnableBluetooth(Context context, BluetoothAdapter adapter) throws BluetoothNotEnabledException, BluetoothNotSupportedException {
         if (adapter != null) {
             if (!adapter.isEnabled()) {
                 Toaster.iToast(context, R.string.enable_bluetooth);
+                //start the adapter
                 boolean enableStarted = adapter.enable();
                 boolean enabled = false;
+                //we need to wait a bit for the enable to take effect...in this case, we poll the adapter's isEnabled method
+                // after some delay and check to see if the adapter has been enabled by a set amount of time
+                //NOTE: these constant times were picked somewhat randomly...the process should never take more than 5 seconds, and
+                // a 500ms delay between polls should be ok in this case.
                 if (enableStarted) {
                     //once the enable process is started, it takes some time to be enabled
-                    int retrySeconds = 5;
-                    int retryPauseMS = 500;
-                    int retries = (retrySeconds * 1000) / retryPauseMS;
+                    int retries = (ENABLE_CHECK_RETRY_SECONDS * 1000) / ENABLE_CHECK_RETRY_PAUSE_MILLISECONDS;
                     int retry = 0;
                     do {
                         try {
-                            Thread.sleep(retryPauseMS);
+                            Thread.sleep(ENABLE_CHECK_RETRY_PAUSE_MILLISECONDS);
                         } catch (InterruptedException e) {
                             //nothing to do
                         }
@@ -34,7 +43,6 @@ public class BluetoothUtils {
                             
                     } while (!enabled && retry++ < retries);                    
                 }
-                //TODO: do we need to wait until it's started here?  adapter.enable just says it's "starting up"
                 if (!enabled) {
                     throw new BluetoothNotEnabledException();
                 }
