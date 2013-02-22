505ce98380322e4f2d143bbc653b722f08351369
Reid Baker <hamiltonbaker@yahoo.com>
2013-02-21 21:27:22 -0500

    add service to manifest and create music service file

---
+++ fanClub/src/com/lastcrusade/fanclub/service/MusicLibrary.java
@@ -0,0 +1,38 @@
+package com.lastcrusade.fanclub.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MusicLibrary extends IntentService {
+
+    public MusicLibrary(){
+        super("MusicLibrary");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        long endTime = System.currentTimeMillis() + 5*1000;
+        while (System.currentTimeMillis() < endTime) {
+            synchronized (this) {
+                try {
+                    wait(endTime - System.currentTimeMillis());
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
+    
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+        return super.onStartCommand(intent,flags,startId);
+    }
+
+    @Override
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "service stopping", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+}
