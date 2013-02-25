39ae8b48436e6ecd0bd6477a0f6b6b2c947cf389
Elizabeth Johnson <ejohnson44@gatech.edu>
2013-02-24 21:19:29 -0500

    beginnings of menu drawer functionality

--- fanClub/src/com/lastcrusade/fanclub/PlaylistActivity.java
+++ fanClub/src/com/lastcrusade/fanclub/PlaylistActivity.java
@@ -1,14 +1,15 @@
 package com.lastcrusade.fanclub;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.actionbarsherlock.app.SherlockActivity;
 import com.lastcrusade.fanclub.model.Song;
 import com.lastcrusade.fanclub.model.SongMetadata;
 import com.lastcrusade.fanclub.util.MusicListAdapter;
+import com.slidingmenu.lib.SlidingMenu;
 
-public class PlaylistActivity extends Activity {
+public class PlaylistActivity extends SherlockActivity {
     //for testing purposes so we have songs to show
     Song[] songs = new Song[]{
             new Song(),
@@ -17,8 +18,10 @@ public class PlaylistActivity extends Activity {
     };
 
   
+    private SlidingMenu menu;
     @Override
     public void onCreate(Bundle savedInstanceState){
+        
         super.onCreate(savedInstanceState);
         
         //for testing purposes
@@ -33,8 +36,19 @@ public class PlaylistActivity extends Activity {
          songs[1].getMetadata().setUsername("Lizziemom");
          songs[2].getMetadata().setUsername("Greenie");
          
+        
+        
+
         setContentView(R.layout.activity_playlist);
         ListView playlist = (ListView)findViewById(R.id.playlistView);
         playlist.setAdapter(new MusicListAdapter(this,songs));
+        
+        menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setBehindOffset(30);
+        menu.setMenu(R.layout.menu);
     }
 }
