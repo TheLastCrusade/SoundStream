dcc4e5ffe0160ad00e5d0137e07704dd3b0bb140
David Greenhalgh <david@david-ThinkPad-X220.(none)>
2013-02-27 12:29:48 -0500

    Serialized LibraryMessage

--- fanClub-test/src/com/lastcrusade/fanclub/net/message/LibraryMessageTest.java
+++ fanClub-test/src/com/lastcrusade/fanclub/net/message/LibraryMessageTest.java
@@ -5,21 +5,30 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 import com.lastcrusade.fanclub.PlaylistFragment;
+import com.lastcrusade.fanclub.model.SongMetadata;
 import com.lastcrusade.fanclub.net.message.LibraryMessage;
 
 //import com.lastcrusade.fanclub.service.MusicLibraryService;
 
-public class LibraryMessageTest extends SerializationTest<LibraryMessage>{
+public class LibraryMessageTest extends SerializationTest<LibraryMessage> {
+	
+	static List<SongMetadata> metadataList = new ArrayList<SongMetadata>(
+            Arrays.asList(new SongMetadata(), new SongMetadata(), new SongMetadata()));
 	
 	@Test
 	public void testSerializeLibraryMessage() throws IOException {
 		LibraryMessage oldMessage = new LibraryMessage();
+		oldMessage.setLibraryString(metadataList.toString());
 		LibraryMessage newMessage = super.testSerializeMessage(oldMessage);
-		
-		//assertEquals( , newMessage.getLibraryString());
+				
+		assertEquals(metadataList.toString() , newMessage.getLibraryString());
 	}
 
 }
