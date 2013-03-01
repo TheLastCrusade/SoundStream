1a1a4bea30c5e56b58b86776bc0e4ddd3cbcaf44
Reid Baker <hamiltonbaker@yahoo.com>
2013-03-01 04:09:21 -0500

    Music library fragment set up to recieve broadcast intent, fix issues when screen is rotated

--- fanClub/src/com/lastcrusade/fanclub/service/MusicLibraryService.java
+++ fanClub/src/com/lastcrusade/fanclub/service/MusicLibraryService.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import com.lastcrusade.fanclub.library.MediaStoreWrapper;
 import com.lastcrusade.fanclub.model.SongMetadata;
+import com.lastcrusade.fanclub.util.BluetoothUtils;
+import com.lastcrusade.fanclub.util.BroadcastIntent;
+import com.lastcrusade.fanclub.util.BroadcastRegistrar;
 
 import android.app.Service;
 import android.content.Intent;
@@ -12,11 +15,17 @@ import android.os.Binder;
 import android.os.IBinder;
 
 public class MusicLibraryService extends Service {
-    List<SongMetadata> metadataList = new ArrayList<SongMetadata>();
+    /**
+     * Broadcast action sent when the MusicLibrary gets or loses music
+     *
+     */
+    public static final String ACTION_LIBRARY_UPDATED = MusicLibraryService.class
+            .getName() + ".action.LibraryUpdated";
 
-    private final IBinder mBinder = new MusicLibraryServiceBinder();
+    private List<SongMetadata> metadataList = new ArrayList<SongMetadata>();
 
-    public class MusicLibraryServiceBinder extends Binder {
+    public class MusicLibraryServiceBinder extends Binder implements
+        ILocalBinder<MusicLibraryService> {
         public MusicLibraryService getService() {
             // Return this instance of LocalService so clients can call public methods
             return MusicLibraryService.this;
@@ -24,15 +33,25 @@ public class MusicLibraryService extends Service {
     }
 
     @Override
+    public void onCreate() {
+        metadataList = (new MediaStoreWrapper(this)).list();
+        String bluetoothName = BluetoothUtils.getLocalBluetoothName();
+        for (SongMetadata song : metadataList) {
+            // Should we store the username/bluetooth name somewhere
+            song.setUsername(bluetoothName);
+        }
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
-        return mBinder;
+        return new MusicLibraryServiceBinder();
     }
     
     /** Methods for clients */
 
     public List<SongMetadata> getLibrary(){
         //TODO implement a way to get updates with library Message
-        return (new MediaStoreWrapper(this)).list();
+        return metadataList;
     }
 
     //untested
@@ -40,6 +59,7 @@ public class MusicLibraryService extends Service {
         for (SongMetadata song : additionalSongs) {
             metadataList.add(song);
         }
+        new BroadcastIntent(ACTION_LIBRARY_UPDATED).send(this);
     }
 
 }
