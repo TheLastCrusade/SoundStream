8c75080cfd1ede4fce81776f8e19483552643084
ejohnson44 <ejohnson44@gatech.edu>
2013-03-17 15:02:07 -0400

    implemented sorting music library alphabetically

--- SoundStream/src/com/lastcrusade/soundstream/service/MusicLibraryService.java
+++ SoundStream/src/com/lastcrusade/soundstream/service/MusicLibraryService.java
@@ -114,8 +114,13 @@ public class MusicLibraryService extends Service {
 
     public List<SongMetadata> getLibrary() {
         synchronized(metadataMutex) {
+            
+            //sorts the list before returning it - for now,
+            //order is simply alphabetical
+            ArrayList<SongMetadata> music = new ArrayList<SongMetadata>(metadataList);
+            Collections.sort(music);
             //unmodifiable copy, for safety
-            return Collections.unmodifiableList(new ArrayList<SongMetadata>(metadataList));
+            return Collections.unmodifiableList(music);
         }
     }
 
