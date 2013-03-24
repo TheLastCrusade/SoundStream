21c676d797787f75bad5148a870eaa83dee05615
David Greenhalgh <david@david-ThinkPad-X220.(none)>
2013-03-24 01:52:56 -0400

    Added script to add GPL statement to the top of every source file

---
+++ utilities/addGPL.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# This script is designed to add our GPL statement to the top of all of our
+# source files
+
+# Java source files
+for i in ../SoundStream*/src/com/lastcrusade/soundstream/*/*.java
+do
+	if ! grep -q Copyright\ 2013\ The\ Last\ Crusade\ ContactLastCrusade@gmail.com $i
+	then
+		cat ../docs/copyrightJava.txt $i > $i.new && mv $i.new $i
+	fi
+done
+
+# XML source files
+for i in ../SoundStream*/res/*/*.xml
+do
+	if ! grep -q Copyright\ 2013\ The\ Last\ Crusade\ ContactLastCrusade@gmail.com $i
+	then
+		cat ../docs/copyrightXML.txt $i > $i.new && mv $i.new $i
+	fi
+done
+
+# Edge case: /net/message
+for i in ../SoundStream*/src/com/lastcrusade/soundstream/*/*/*.java
+do
+	if ! grep -q Copyright\ 2013\ The\ Last\ Crusade\ ContactLastCrusade@gmail.com $i
+	then
+		cat ../docs/copyrightJava.txt $i > $i.new && mv $i.new $i
+	fi
+done
+
+# Edge case: AndroidManifest.xml and build.xml
+for i in ../SoundStream*/*.xml
+do
+	if ! grep -q Copyright\ 2013\ The\ Last\ Crusade\ ContactLastCrusade@gmail.com $i
+	then
+		cat ../docs/copyrightXML.txt $i > $i.new && mv $i.new $i
+	fi
+done
+
+# Edge case: SoundStream-test Parcelable.java
+for i in ../SoundStream*/src/android/os/*.java
+do
+	if ! grep -q Copyright\ 2013\ The\ Last\ Crusade\ ContactLastCrusade@gmail.com $i
+	then
+		cat ../docs/copyrightJava.txt $i > $i.new && mv $i.new $i
+	fi
+done
