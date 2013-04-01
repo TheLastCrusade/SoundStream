36e9b05ca2b03e5bbe3ba0e8dca765886a87e5a7
David Greenhalgh <david@david-ThinkPad-X220.(none)>
2013-04-01 14:54:11 -0400

    Cleared out PlayStatusMessageTest

--- SoundStream-test/src/com/lastcrusade/soundstream/net/message/PlayStatusMessageTest.java
+++ SoundStream-test/src/com/lastcrusade/soundstream/net/message/PlayStatusMessageTest.java
@@ -19,39 +19,12 @@
 
 package com.lastcrusade.soundstream.net.message;
 
-import static com.lastcrusade.soundstream.util.CustomAssert.assertSongMetaEquals;
-import static org.junit.Assert.assertEquals;
-
-import java.io.IOException;
-
 import org.junit.Test;
-
-import com.lastcrusade.soundstream.model.SongMetadata;
 
 public class PlayStatusMessageTest extends SerializationTest<PlayStatusMessage> {
 	
     @Test
     public void testSerializePlayStatusMessage() throws Exception {
-        PlayStatusMessage preSerializationPlayStatusMsg = new PlayStatusMessage();
-
-        preSerializationPlayStatusMsg.setString(PlayStatusMessage.PLAY_MESSAGE);
-        preSerializationPlayStatusMsg.setCurrentSong(getSomeSongMetadata());
-        PlayStatusMessage postSerializationPlayStatusMsg = super.testSerializeMessage(preSerializationPlayStatusMsg);
-		
-        // Testing play with song
-        assertEquals(PlayStatusMessage.PLAY_MESSAGE, postSerializationPlayStatusMsg.getString());
-        assertSongMetaEquals(getSomeSongMetadata(), postSerializationPlayStatusMsg.getCurrentSong());
-
-        // Testing pause with song
-        preSerializationPlayStatusMsg.setString(PlayStatusMessage.PAUSE_MESSAGE);
-	    preSerializationPlayStatusMsg.setCurrentSong(getSomeSongMetadata());
-	    postSerializationPlayStatusMsg = super.testSerializeMessage(preSerializationPlayStatusMsg);
-	    assertEquals(PlayStatusMessage.PAUSE_MESSAGE, postSerializationPlayStatusMsg.getString());
-	    assertSongMetaEquals(getSomeSongMetadata(), postSerializationPlayStatusMsg.getCurrentSong());
-	}
-	
-	private SongMetadata getSomeSongMetadata(){
-	    return new SongMetadata(69, "Driver that Had a Dick on His Shoulder",
-	            "Aziz Ansari", "Dangerously Delicious", 2345, "David");
+        assert(false);
 	}
 }
