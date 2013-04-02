5ed34f07eb20af998fc14b86500e51faea7ebb16
Bryan Sills <bryan.a.sills@gmail.com>
2013-04-02 01:28:31 -0400

    removed non-functional code

--- SoundStream/src/com/lastcrusade/soundstream/components/NetworkFragment.java
+++ SoundStream/src/com/lastcrusade/soundstream/components/NetworkFragment.java
@@ -25,7 +25,6 @@ import com.lastcrusade.soundstream.net.message.FoundGuest;
 import com.lastcrusade.soundstream.service.ConnectionService;
 import com.lastcrusade.soundstream.service.ConnectionService.ConnectionServiceBinder;
 import com.lastcrusade.soundstream.service.MusicLibraryService;
-import com.lastcrusade.soundstream.service.MusicLibraryService.MusicLibraryServiceBinder;
 import com.lastcrusade.soundstream.service.PlaylistService;
 import com.lastcrusade.soundstream.service.PlaylistService.PlaylistServiceBinder;
 import com.lastcrusade.soundstream.service.ServiceLocator;
@@ -166,21 +165,6 @@ public class NetworkFragment extends SherlockFragment implements ITitleable {
                 }
             }
         });
-
-        musicLibraryServiceLocator = new ServiceLocator<MusicLibraryService>(
-                this.getActivity(), MusicLibraryService.class, MusicLibraryServiceBinder.class);
-        musicLibraryServiceLocator.setOnBindListener(new ServiceLocator.IOnBindListener() {
-            @Override
-            public void onServiceBound() {
-                try {
-                    musicLibraryServiceLocator.getService().clearExternalMusic();
-                } catch (ServiceNotBoundException e) {
-                    Log.wtf(TAG,"Service not bound");
-                }
-            }
-        });
-
-        ((CustomApp)getActivity().getApplication()).setUserList(new UserList());
 
         Transitions.transitionToConnect((CoreActivity) getActivity());
     }
