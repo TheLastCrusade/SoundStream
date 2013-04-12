7ac4f9e095b003cc2e9de47fb7ccc9d8aa1b6a30
Bryan Sills <bryan.a.sills@gmail.com>
2013-04-11 20:21:39 -0400

    UI tests

---
+++ uitests/addsongandplay.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env monkeyrunner
+
+from uiutils import *
+
+init()
+
+touchWithCD('Create')
+touchWithCD('Music Library')
+touchWithCD('Add To Playlist_Avalanche Rock')
+touchWithCD('Navigate up')
+touchWithCD('Playlist')
+touchWithCD('Play/Pause')
--- uitests/uiutils.py
+++ uitests/uiutils.py
@@ -3,48 +3,65 @@ import sys
 import os
 import getopt
 
-# This must be imported before MonkeyRunner and MonkeyDevice,
-# otherwise the import fails.
-# PyDev sets PYTHONPATH, use it
-try:
-    for p in os.environ['PYTHONPATH'].split(':'):
-        if not p in sys.path:
-            sys.path.append(p)
-except:
-    pass
-    
-try:
-    sys.path.append(os.path.join(os.environ['ANDROID_VIEW_CLIENT_HOME'], 'src'))
-except:
-    pass
-
-from com.dtmilano.android.viewclient import ViewClient
+vc = None
+device = None
 
 def usage(exitVal=1):
-    print >> sys.stderr, 'usage: dump.py [-H|--%s] [-V|--%s] [-I|--%s] [-F|--%s] [-S|--%s] [-u|--%s] [-x|--%s] [-d|--%s] [-c|--%s] [serialno]' % \
-        tuple(LONG_OPTS)
+    print >> sys.stderr, 'usage: <test-name>.py [-d|--device]'
     sys.exit(exitVal)
 
-#try:
-#    opts, args = getopt.getopt(sys.argv[1:], 'HVIFSuxdc', LONG_OPTS)
-#except getopt.GetoptError, e:
-#    print >>sys.stderr, 'ERROR:', str(e)
-#    usage()
+def setup():
+    # This must be imported before MonkeyRunner and MonkeyDevice,
+    # otherwise the import fails.
+    # PyDev sets PYTHONPATH, use it
+    try:
+        for p in os.environ['PYTHONPATH'].split(':'):
+            if not p in sys.path:
+                sys.path.append(p)
+    except:
+        pass
+    
+    try:
+        sys.path.append(os.path.join(os.environ['ANDROID_VIEW_CLIENT_HOME'], 'src'))
+    except:
+        pass
 
-kwargs1 = {'verbose': True, 'ignoresecuredevice': False}
-kwargs2 = {'forceviewserveruse': False, 'startviewserver': True}
+def parseArgs():
+    deviceArg = None
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hd:", ["device="])
+    except getopt.GetoptError:
+        usage()
 
-vc = ViewClient(*ViewClient.connectToDeviceOrExit(**kwargs1), **kwargs2)
-vc.traverse(transform=ViewClient.TRAVERSE_CITCD)
-vc.findViewWithContentDescriptionOrRaise('Create').touch()
-vc.dump()
-vc.traverse(transform=ViewClient.TRAVERSE_CITCD)
-vc.findViewWithContentDescriptionOrRaise('Navigate up').touch()
-vc.findViewWithTextOrRaise('Music Library').touch()
-vc.dump()
-vc.traverse(transform=ViewClient.TRAVERSE_CITCD)
-vc.findViewByIdOrRaise('id/btn_add_to_playlist').touch()
-vc.findViewByIdOrRaise('id/home').touch()
-vc.findViewWithTextOrRaise('Playlist').touch()
-vc.dump()
-vc.findViewByIdOrRaise('id/btn_play_pause').touch()
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+        elif opt in ("-d", "--device"):
+            deviceArg = arg
+
+    return deviceArg
+
+def connect(deviceID = 'emulator-5554'):
+    from com.dtmilano.android.viewclient import ViewClient
+
+    kwargs1 = {'verbose': True, 'serialno': deviceID}
+    kwargs2 = {'forceviewserveruse': False, 'startviewserver': True}
+
+    device, serialno = ViewClient.connectToDeviceOrExit(**kwargs1)
+    vc = ViewClient(device, serialno, **kwargs2)
+
+def init():
+    setup()
+    deviceArg = parseArgs()
+    if (deviceArg is not None):
+        connect(deviceArg)
+    else:
+        connect()
+
+
+def printLayout():
+    vc.traverse(transform=ViewClient.TRAVERSE_CITCD)
+
+def touchWithCD(contentDescription):
+    vc.findViewWithContentDescriptionOrRaise(contentDescription).touch()
+    vc.dump()
