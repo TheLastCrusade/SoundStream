e6812def433617241951558014d7d68371700242
Bryan Sills <bryan.a.sills@gmail.com>
2013-04-28 13:27:59 -0400

    pretty pretty code

--- SoundStream/src/com/lastcrusade/soundstream/util/MusicListAdapter.java
+++ SoundStream/src/com/lastcrusade/soundstream/util/MusicListAdapter.java
@@ -84,8 +84,8 @@ public class MusicListAdapter<T extends SongMetadata> extends BaseAdapter {
         
                View userColor = (View) element.findViewById(R.id.user_color); 
                TextView title = (TextView) element.findViewById(R.id.title);
-              TextView artist = (TextView) element.findViewById(R.id.artist);
                TextView album = (TextView) element.findViewById(R.id.album);
+              TextView artist = (TextView) element.findViewById(R.id.artist);
         ImageButton addButton = (ImageButton) element.findViewById(R.id.btn_add_to_playlist);
 
         String macAddress = metadataList.get(position).getMacAddress();
