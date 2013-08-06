27e93151a874708def9ec0b573cafcf076473d9d
David Greenhalgh <david@david-ThinkPad-X220.(none)>
2013-08-06 01:41:49 -0400

    About page additions and link to repo

--- SoundStream/src/com/lastcrusade/soundstream/components/AboutFragment.java
+++ SoundStream/src/com/lastcrusade/soundstream/components/AboutFragment.java
@@ -18,10 +18,14 @@
  */
 package com.lastcrusade.soundstream.components;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.lastcrusade.soundstream.CoreActivity;
@@ -39,6 +43,17 @@ public class AboutFragment extends SherlockFragment implements ITitleable {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_about, container, false);
+        
+        final TextView repoLinkText = (TextView)v.findViewById(R.id.repo_link);
+        
+        repoLinkText.setOnClickListener(new AdapterView.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.github.com/TheLastCrusade/SoundStream"));
+		        startActivity(i);
+			}
+		});
         
         return v;
     }
